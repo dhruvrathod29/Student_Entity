bcd576dd0b8ddeab4aa8c5d72f90b0ed05c3532a
Dhruv Rathod <86467492+dhruvrathod29@users.noreply.github.com>
2022-12-14 00:57:27 +0530

    'Student_Entity_Data','StudentDbContext'

---
+++ Student_Entity/Data/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Entity.Models;
+
+namespace Student_Entity.Data
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) 
+        { 
+            
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        // migration command == add-migration "Comment"
+        //Update database command == update-database
+
+        //Connection String 
+    }
+}
